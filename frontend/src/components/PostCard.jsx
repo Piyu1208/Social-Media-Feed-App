c09984f2335c03c76bda09310ee58977fd637ceb
Piyush Sharma <piyu22551@gmail.com>
2026-07-24 19:14:19 +0530

    added onClick to comment btn.

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -13,6 +13,7 @@ import {
 
 import { Heart, MessageCircle } from "lucide-react";
 import api from "../api/axios.js";
+import { useNavigate } from "react-router-dom";
 
 export default function PostCard({
   id,
@@ -28,6 +29,16 @@ export default function PostCard({
   const [liked, setLiked] = useState(isLiked);
   const [likeCount, setLikeCount] = useState(likes);
 
+  const [currentImage, setCurrentImage] = useState(0);
+  const scrollRef = useRef(null);
+
+  const navigate = useNavigate();
+
+  const handleScroll = (e) => {
+    const { scrollLeft, clientWidth } = e.target;
+    setCurrentImage(Math.round(scrollLeft / clientWidth));
+  };
+
   const handleLike = async () => {
     const newLiked = !liked;
 
@@ -36,7 +47,6 @@ export default function PostCard({
 
     try {
       await api.patch(`/posts/${id}/like`);
-
     } catch (err) {
       setLiked(!newLiked);
       setLikeCount((count) => count + (newLiked ? -1 : 1));
@@ -68,8 +78,15 @@ export default function PostCard({
         </CardAction>
       </CardHeader>
 
-      <CardContent className="p-0">
-        <div className="flex snap-x snap-mandatory overflow-x-auto scrollbar-hide">
+      <CardContent className="relative p-0">
+        <div
+          ref={scrollRef}
+          onScroll={handleScroll}
+          className="flex snap-x snap-mandatory overflow-x-auto overflow-y-hidden scroll-smooth
+               [-ms-overflow-style:none]
+               [scrollbar-width:none]
+               [&::-webkit-scrollbar]:hidden"
+        >
           {images.map((image) => (
             <img
               key={image.public_id}
@@ -79,6 +96,19 @@ export default function PostCard({
             />
           ))}
         </div>
+
+        {images.length > 1 && (
+          <div className="absolute bottom-3 left-1/2 flex -translate-x-1/2 gap-1.5">
+            {images.map((_, index) => (
+              <div
+                key={index}
+                className={`h-2 w-2 rounded-full transition-all duration-200 ${
+                  currentImage === index ? "bg-white" : "bg-white/40"
+                }`}
+              />
+            ))}
+          </div>
+        )}
       </CardContent>
 
       <CardFooter className="flex flex-col items-start gap-4 p-4">
@@ -99,7 +129,9 @@ export default function PostCard({
           </div>
 
           <div className="flex items-center gap-2">
-            <Button variant="ghost" size="icon">
+            <Button variant="ghost" size="icon"
+            onClick={(e) => navigate(`/post/${id}`)}
+            >
               <MessageCircle className="h-5 w-5" />
             </Button>
 
