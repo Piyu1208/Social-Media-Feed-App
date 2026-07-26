987f14e6a9ae42c70d1fe7b4002b505ffb9dcecc
Piyush Sharma <piyu22551@gmail.com>
2026-07-26 21:57:53 +0530

    crop image util added.

---
+++ frontend/src/utils/cropImage.js
@@ -0,0 +1,47 @@
+export const createImage = (url) =>
+  new Promise((resolve, reject) => {
+    const image = new Image();
+    image.addEventListener("load", () => resolve(image));
+    image.addEventListener("error", reject);
+    image.src = url;
+  });
+
+export async function getCroppedImage(imageSrc, pixelCrop) {
+  const image = await createImage(imageSrc);
+
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+
+  canvas.width = pixelCrop.width;
+  canvas.height = pixelCrop.height;
+
+  ctx.drawImage(
+    image,
+    pixelCrop.x,
+    pixelCrop.y,
+    pixelCrop.width,
+    pixelCrop.height,
+    0,
+    0,
+    pixelCrop.width,
+    pixelCrop.height,
+  );
+
+  return new Promise((resolve, reject) => {
+    canvas.toBlob((blob) => {
+      if (!blob) {
+        reject(new Error("Failed to create image"));
+        return;
+      }
+
+      const file = new File([blob], "cropped.jpg", {
+        type: "image/jpeg",
+      });
+
+      resolve({
+        file,
+        url: URL.createObjectURL(file),
+      });
+    }, "image/jpeg");
+  });
+}
