1b6fd8aa3afe31050c20bd4240837261d79c8067
Piyush Sharma <piyu22551@gmail.com>
2026-08-05 23:18:22 +0530

    added hover color to navigation icons.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,19 +14,19 @@ export default function Navbar() {
 
         <div className="flex items-center gap-4">
           <Link to="/">
-            <House className="h-5 w-5" />
+            <House className="h-5 w-5 hover:text-gray-300" />
           </Link>
 
           <Link to="/search">
-            <Search className="h-5 w-5" />
+            <Search className="h-5 w-5 hover:text-gray-300" />
           </Link>
 
           <Link to="/create-post">
-            <PlusSquare className="h-5 w-5" />
+            <PlusSquare className="h-5 w-5 hover:text-gray-300" />
           </Link>
 
           <Link to="/notifications" className="relative">
-            <Bell className="h-5 w-5" />
+            <Bell className="h-5 w-5 hover:text-gray-300" />
 
             {unreadCount > 0 && (
               <span className="absolute -right-2 -top-2 flex h-5 min-w-[20px] items-center justify-center rounded-full bg-red-500 px-1 text-xs font-bold text-white">
@@ -36,7 +36,7 @@ export default function Navbar() {
           </Link>
 
           <Link to="/profile">
-            <User className="h-5 w-5" />
+            <User className="h-5 w-5 hover:text-gray-300" />
           </Link>
         </div>
       </div>
